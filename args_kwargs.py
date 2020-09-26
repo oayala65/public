59c11cae98bef54863ac7a237c248f50f4ff6d87
Osvaldo Ayala <oayala65@gmail.com>
2020-09-26 19:04:45 -0400

    comit de total

--- args_kwargs.py
+++ args_kwargs.py
@@ -4,6 +4,7 @@ def super_func(*args,**kwargs):
   total=0
   for items in kwargs.values():
     total +=items
+    print(total)
   return sum(args)+ total 
   
 print(super_func(1,2,3,4,5, num1=5, num2=10))
