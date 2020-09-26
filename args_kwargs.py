bb52dbc196911cde4f6079bf1388ae750816e148
Osvaldo Ayala <oayala65@gmail.com>
2020-09-26 10:33:16 -0400

    Initial commit

---
+++ args_kwargs.py
@@ -0,0 +1,9 @@
+def super_func(*args,**kwargs):
+  print(f'*args {args}')
+  print(f'**kwargs {kwargs}')
+  total=0
+  for items in kwargs.values():
+    total +=items
+  return sum(args)+ total 
+  
+print(super_func(1,2,3,4,5, num1=5, num2=10))
